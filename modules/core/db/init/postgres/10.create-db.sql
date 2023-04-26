f84c97a57967f391d9926c41e1e79516892d8f0e
Florin Tanasa <florin.tanasa@svgenebank.ro>
2023-04-26 08:40:24 +0300

    feat: Create table for entity Taxonomic_units

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -33,3 +33,19 @@ create table TAXONOMY_GEOGRAPHIC_DIV (
     primary key (ID)
 )^
 -- end TAXONOMY_GEOGRAPHIC_DIV
+-- begin TAXONOMY_TAXONOMIC_UNITS
+create table TAXONOMY_TAXONOMIC_UNITS (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    TSN integer not null,
+    --
+    primary key (ID)
+)^
+-- end TAXONOMY_TAXONOMIC_UNITS
