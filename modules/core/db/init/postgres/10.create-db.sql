b42563922972ef96fc393913cfb51611538844c4
Florin Tanasa <florin.tanasa@svgenebank.ro>
2023-04-27 08:41:12 +0300

    feat: Create table Longnames

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -69,3 +69,21 @@ create table TAXONOMY_TU_COMMENTS_LINKS (
     primary key (ID)
 )^
 -- end TAXONOMY_TU_COMMENTS_LINKS
+-- begin TAXONOMY_LONGNAMES
+create table TAXONOMY_LONGNAMES (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    TSN integer not null,
+    COMPLETENAME varchar(300) not null,
+    ID_TAXONOMIC_UNITS_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end TAXONOMY_LONGNAMES
