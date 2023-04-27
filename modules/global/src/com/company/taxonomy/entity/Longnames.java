419044d78b179411618e899866fd601b4318ec8a
Florin Tanasa <florin.tanasa@svgenebank.ro>
2023-04-27 08:37:55 +0300

    feat: Create entity Longnames

---
+++ modules/global/src/com/company/taxonomy/entity/Longnames.java
@@ -0,0 +1,56 @@
+package com.company.taxonomy.entity;
+
+import com.haulmont.chile.core.annotations.Composition;
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+import com.haulmont.cuba.core.entity.annotation.OnDelete;
+import com.haulmont.cuba.core.global.DeletePolicy;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Table(name = "TAXONOMY_LONGNAMES")
+@Entity(name = "taxonomy_Longnames")
+@NamePattern("%s|completename")
+public class Longnames extends StandardEntity {
+    private static final long serialVersionUID = -6372691847896593621L;
+
+    @NotNull
+    @Column(name = "TSN", nullable = false)
+    private Integer tsn;
+
+    @NotNull
+    @Column(name = "COMPLETENAME", nullable = false)
+    private String completename;
+
+    @NotNull
+    @Composition
+    @OnDelete(DeletePolicy.CASCADE)
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "ID_TAXONOMIC_UNITS_ID")
+    private Taxonomic_units id_taxonomic_units;
+
+    public Integer getTsn() {
+        return tsn;
+    }
+
+    public void setTsn(Integer tsn) {
+        this.tsn = tsn;
+    }
+
+    public String getCompletename() {
+        return completename;
+    }
+
+    public void setCompletename(String completename) {
+        this.completename = completename;
+    }
+
+    public Taxonomic_units getId_taxonomic_units() {
+        return id_taxonomic_units;
+    }
+
+    public void setId_taxonomic_units(Taxonomic_units id_taxonomic_units) {
+        this.id_taxonomic_units = id_taxonomic_units;
+    }
+}
